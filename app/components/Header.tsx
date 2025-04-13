0a2b4bedd0e03d4338d2db2e945724439347c61c
SajanKumarSingh <kumarsajan0013@gmail.com>
2025-04-14 00:46:56 +0530

    error 0.3

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
+import Image from "next/image";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
