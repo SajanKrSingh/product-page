397a9b242753ec5ec4613e9b3129da28c582d02c
SajanKumarSingh <kumarsajan0013@gmail.com>
2025-04-14 00:30:04 +0530

    fixed the error

--- app/page.tsx
+++ app/page.tsx
@@ -50,6 +50,7 @@ export default async function Home({
             </div>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 border border-gray-200">
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
               {products.map((product: any) => (
                 <ProductCard key={product.id} product={product} />
               ))}
